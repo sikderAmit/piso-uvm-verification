4568b6756632ba10d2f7844393d9706829d40e94
sikderamit <amit.sder@gmail.com>
2024-01-28 16:33:47 +0600

    packege file modified

---
+++ piso_sv_files/piso_pkg.sv
@@ -0,0 +1,15 @@
+//tb-files including
+package piso_pkg;
+    piso_intf.sv
+    piso_seq_item.sv
+    piso_sequence.sv
+
+    piso_mntr.sv
+    piso_drvr.sv
+    piso_predictor.sv
+    piso_scb.sv
+    piso_agnt.sv
+    piso_env.sv
+
+    piso_test.sv
+endpackage
